0a8542de28aed0850d57219c69f80e3e6611f78e
Gabriel Garcia <gabriel_hg58@hotmail.com>
2018-11-06 19:50:49 -0200

    Second

---
+++ build/precache-manifest.64afab8a164472b4ae29a45f3a74da76.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dc8f03ad4ccf11fdbbb3860c0568e8d",
+    "url": "/Website-ReactJS/static/media/img-02.4dc8f03a.jpg"
+  },
+  {
+    "revision": "d28c62dc5a7d3ea03b39",
+    "url": "/Website-ReactJS/static/css/main.b48592f2.chunk.css"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/Website-ReactJS/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "4380b31da00a8cb8cc24",
+    "url": "/Website-ReactJS/static/js/runtime~main.4380b31d.js"
+  },
+  {
+    "revision": "7d494fd9bcb34eb794d7a0aeabca157f",
+    "url": "/Website-ReactJS/static/media/perfil.7d494fd9.jpg"
+  },
+  {
+    "revision": "06980af5425a982deb9f6ac654d7ef87",
+    "url": "/Website-ReactJS/static/media/img-03.06980af5.jpg"
+  },
+  {
+    "revision": "d28c62dc5a7d3ea03b39",
+    "url": "/Website-ReactJS/static/js/main.d28c62dc.chunk.js"
+  },
+  {
+    "revision": "cd8bf794efd68954d15abacaf7685d13",
+    "url": "/Website-ReactJS/static/media/img-01.cd8bf794.jpg"
+  },
+  {
+    "revision": "b0da250902c677dff4621cfaf249854d",
+    "url": "/Website-ReactJS/static/media/facebook_logo.b0da2509.png"
+  },
+  {
+    "revision": "f2751cc1fc6fc7dc6cac1fc7e67ec944",
+    "url": "/Website-ReactJS/static/media/logo_github.f2751cc1.png"
+  },
+  {
+    "revision": "18b09ec2b04df6d7ed285227147a0ada",
+    "url": "/Website-ReactJS/static/media/mail_logo.18b09ec2.png"
+  },
+  {
+    "revision": "4457f223217bf2e25410db710b09b862",
+    "url": "/Website-ReactJS/static/media/instagram_logo.4457f223.png"
+  },
+  {
+    "revision": "0bcc6ba85f7306ffba077635b79c202c",
+    "url": "/Website-ReactJS/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Website-ReactJS/precache-manifest.64afab8a164472b4ae29a45f3a74da76.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Website-ReactJS/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
